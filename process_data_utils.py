8b9a680876157589f35df893811ab0ccf6465e4e
shaemarks <shaemarks34@gmail.com>
2023-11-21 14:32:37 -0500

    Experiment with NLTK's Ngram models

--- process_data_utils.py
+++ process_data_utils.py
@@ -1,5 +1,6 @@
 import nltk
 import csv
+import pandas as pd
 
 nltk.download('punkt')
 
@@ -12,45 +13,66 @@ def clean_artist_name(name: str):
     name = name.replace('-', ' ')
     name = name.replace('/', '')
     return name
-                   
-# NOTE: Below included for reference. It will be modified for this project.
-# def tokenize_line(line: str, ngram: int, 
-#                    by_char: bool = True, 
-#                    space_char: str = ' ',
-#                    sentence_begin: str=SENTENCE_BEGIN, 
-#                    sentence_end: str=SENTENCE_END):
-#   """
-#   Tokenize a single string. Glue on the appropriate number of 
-#   sentence begin tokens and sentence end tokens (ngram - 1), except
-#   for the case when ngram == 1, when there will be one sentence begin
-#   and one sentence end token.
-#   Args:
-#     line (str): text to tokenize
-#     ngram (int): ngram preparation number
-#     by_char (bool): default value True, if True, tokenize by character, if
-#       False, tokenize by whitespace
-#     space_char (str): if by_char is True, use this character to separate to replace spaces
-#     sentence_begin (str): sentence begin token value
-#     sentence_end (str): sentence end token value
 
-#   Returns:
-#     list of strings - a single line tokenized
-#   """
-#   # PROVIDED
-#   inner_pieces = None
-#   if by_char:
-#     line = line.replace(' ', space_char)
-#     inner_pieces = list(line)
-#   else:
-#     # otherwise use nltk's word tokenizer
-#     inner_pieces = nltk.word_tokenize(line)
 
-#   if ngram == 1:
-#     tokens = [sentence_begin] + inner_pieces + [sentence_end]
-#   else:
-#     tokens = ([sentence_begin] * (ngram - 1)) + inner_pieces + ([sentence_end] * (ngram - 1))
-#   # always count the unigrams
-#   return tokens
+def get_lyrics_in_genre(df: pd.DataFrame, genre: str) -> list:
+   """
+   Returns the lyrics of songs in df with the given genre.
+
+   Args:
+      df (pandas DataFrame): dataframe of artist and lyric data
+      genre (str): a music genre found in df
+    Returns:
+
+   """ 
+   genre_df = df[df['genres'].apply(lambda x: genre in x)]
+   return genre_df['lyrics'].tolist()
+
+
+# NOTE: Might not actually need these -- just trying to get things down as I experiment with NLTK's Ngrams 
+def tokenize_line(line: str, ngram: int, 
+                   sentence_begin: str=SENTENCE_BEGIN, 
+                   sentence_end: str=SENTENCE_END):
+  """
+  Tokenize a single string. Glue on the appropriate number of 
+  sentence begin tokens and sentence end tokens (ngram - 1), except
+  for the case when ngram == 1, when there will be one sentence begin
+  and one sentence end token.
+  Args:
+    line (str): text to tokenize
+    ngram (int): ngram preparation number
+    sentence_begin (str): sentence begin token value
+    sentence_end (str): sentence end token value
+
+  Returns:
+    list of strings - a single line tokenized
+  """
+  inner_pieces = nltk.word_tokenize(line)
+
+  if ngram == 1:
+    tokens = [sentence_begin] + inner_pieces + [sentence_end]
+  else:
+    tokens = ([sentence_begin] * (ngram - 1)) + inner_pieces + ([sentence_end] * (ngram - 1))
+  return tokens
+
+
+def generate_ngrams(data: list, ngram: int) -> list:
+    '''
+    Takes the tokenized lyric data (list of lists) and 
+    generates the ngram training samples out of it.
+    Parameters:
+    data: a list of lists 
+    ngram: the size of the ngrams that should be produced 
+    return: 
+    list of lists in the format [[x1, x2, ... , xn], ...]
+    '''
+    ngram_samples = []
+
+    for line in data:
+        for i in range(len(line) - ngram + 1):
+            ngram_samples.append(line[i:i+ngram])
+            
+    return ngram_samples
 
 
 # def read_file(datapath, ngram, by_character=False):
