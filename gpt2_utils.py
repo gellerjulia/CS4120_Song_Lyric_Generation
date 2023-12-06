d323be17a0913b4e071d22f5b3dd0bff49b4acc3
gellerjulia <74266997+gellerjulia@users.noreply.github.com>
2023-12-06 12:19:42 -0500

    m

--- gpt2_utils.py
+++ gpt2_utils.py
@@ -2,6 +2,9 @@ import torch
 import transformers
 from tqdm import tqdm
 from typing import List
+import tensorflow as tf
+import math
+import numpy as np 
 
 # defining Dataset Class
 class Dset(torch.utils.data.Dataset):
@@ -133,51 +136,49 @@ def load_model(file_path):
     
     return loaded_model
 
-def compute_perplexity(model: transformers.GPT2LMHeadModel, tokenizer:transformers.GPT2Tokenizer, test_data: List[str], max_seq_len: int, device: str) -> float:
-    """ Computes model perplexity on test data.
+
+def compute_perplexity(path_to_model: str, tokenizer: transformers.GPT2Tokenizer, test_data: List[str], max_length: int=10) -> float:
+  """ Computes model perplexity on test data.
     Args:
-      model (transformers.GPT2LMHeadModel): finetuned GPT2 model
+      path_to_model: str file path to the saved fine-tuned GPT2 model
       tokenizer (transformers.GPT2Tokenizer): pretrained GPT2 tokenizer
-      test_Data (List of str): the test data in its tokenized form
-      max_seq_len (int): the maximum length of each tokenized datapoint
-      device (str): name of device to run on 
+      test_data (List of str): the test data in its tokenized form
+      max_len (int): the maximum length of each tokenized datapoint
 
     Returns:
-      texts: list of list of str generated texts that are in their tokenized forms
-    """
-    # get encodings for test data
-    encodings = tokenizer("\n\n".join(test_data), return_tensors="tf")
+      ppl: float perplexity of test data
+  """
+  # load the saved model
+  loaded_model = load_model(path_to_model)
 
-    max_length = model.config.n_positions
-    stride = 512
-    seq_len = max_seq_len
+  encodings = tokenizer("\n\n".join(test_data), return_tensors="tf")
 
-    nlls = []
-    prev_end_loc = 0
-    for begin_loc in tqdm(range(0, seq_len, stride)):
-        end_loc = min(begin_loc + max_length, seq_len)
-        trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
-        # input_ids = encodings.input_ids[:, begin_loc:end_loc].to(device)
-        input_ids = encodings.input_ids[:, begin_loc:end_loc]
-        target_ids = input_ids.clone()
-        target_ids[:, :-trg_len] = -100
+  max_length = max_length
+  stride=10
+  seq_len = encodings.input_ids.shape[1]
 
-        with torch.no_grad():
-            outputs = model(input_ids, labels=target_ids)
+  nlls = []
+  prev_end_loc = 0
+  for begin_loc in tqdm(range(0, seq_len, stride)):
+      end_loc = min(begin_loc + max_length, seq_len)
+      trg_len = end_loc - prev_end_loc  # may be different from stride on last loop
+      input_ids = encodings.input_ids[:, begin_loc:end_loc]
+      target_ids_np = tf.identity(input_ids).numpy()
+      target_ids_np[:, :-trg_len] = -100 
+      target_ids = tf.convert_to_tensor(np.array(target_ids_np))
+      with torch.no_grad():
+          outputs = loaded_model(input_ids, labels=target_ids)
 
-            # loss is calculated using CrossEntropyLoss which averages over valid labels
-            # N.B. the model only calculates loss over trg_len - 1 labels, because it internally shifts the labels
-            # to the left by 1.
-            neg_log_likelihood = outputs.loss
+          # loss is calculated using CrossEntropyLoss which averages over valid labels
+          # N.B. the model only calculates loss over trg_len - 1 labels, because it internally shifts the labels
+          # to the left by 1.
+          neg_log_likelihood = outputs.loss
 
-        nlls.append(neg_log_likelihood)
+      nlls.append(neg_log_likelihood)
 
-        prev_end_loc = end_loc
-        if end_loc == seq_len:
-            break
-
-    ppl = torch.exp(torch.stack(nlls).mean())
-    # convert to scalar
-    ppl = ppl.numpy().tolist()
-
-    return ppl
+      prev_end_loc = end_loc
+      if end_loc == seq_len:
+          break
+      
+  ppl = math.exp(np.mean(nlls))
+  return ppl
